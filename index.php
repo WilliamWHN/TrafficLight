eb0aec4581a2e17728b0bfa213441a89f925615a
Hausmann <william.hausmann@cpnv.ch>
2020-09-11 10:23:27 +0200

    the button pause can't be paused anymore if it isn't in the right state

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ $lights->setState($_SESSION['state']);
             <a class ="btn btn-dark d-block" href="/?next=true"><i class="fas fa-step-forward fa-4x"></i></a>
         </div>
         <div class="col-mm m-2 p-2">
-            <a class="btn btn-dark d-block" href="/?next=false"><i class="far fa-stop-circle fa-4x"></i></a>
+             <a class="btn btn-dark d-block <?= $_SESSION['state'] == 1 || $_SESSION['state'] == 3 ? 'disabled' : '' ?>" href="/?next=false"><i class="far fa-stop-circle fa-4x"></i></a>
         </div>
     </div>
 </div>
